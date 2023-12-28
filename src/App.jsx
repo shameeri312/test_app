1a8f59233a27fcf112c952cfc2d94c14d9e0ae7b
shameeri312 <114741290+shameeri312@users.noreply.github.com>
2023-12-28 20:52:02 +0500

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,80 +1,94 @@
-import { useState } from 'react'
-import './App.css'
-import About from './components/About'
-import Alert from './components/Alert'
-import Navbar from './components/Navbar'
-import Textform from './components/Textform'
+import { useState } from "react";
+import "./App.css";
+import About from "./components/About";
+import Alert from "./components/Alert";
+import Navbar from "./components/Navbar";
+import Textform from "./components/Textform";
+import LoadingBar from "react-top-loading-bar";
 import React from "react";
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Home from './components/Home'
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from "./components/Home";
 
 function App() {
-  const [mode, setMode] = useState('light')
-  const [text, setText] = useState('dark')
-  const [alert, setAlert] = useState(null)
+  const [mode, setMode] = useState("light");
+  const [text, setText] = useState("dark");
+  const [alert, setAlert] = useState(null);
   const [myStyle, setMyStyle] = useState({
-    color: 'black',
-    backgroundColor: 'white',
-  })
-
+    color: "black",
+    backgroundColor: "white",
+  });
+  const [progress, setProgress] = useState(0);
   const showAlert = (message, type) => {
     setAlert({
       msg: message,
       type: type,
-    })
+    });
     setTimeout(() => {
-      setAlert(null)
+      setAlert(null);
     }, 3000);
-  }
-
-
+  };
 
   const changeTheme = () => {
-    if (mode === 'light') {
-      setText('white')
-      setMode('dark')
-      document.body.style.backgroundColor = '#001025'
-      document.body.style.color = 'white'
-      showAlert('Dark mode has been enabled!!', 'info')
-      document.title = 'Dark Mode'
+    if (mode === "light") {
+      setText("white");
+      setMode("dark");
+      document.body.style.backgroundColor = "#001025";
+      document.body.style.color = "white";
+      showAlert("Dark mode has been enabled!!", "info");
+      document.title = "Dark Mode";
 
       setMyStyle({
-        color: 'white',
-        backgroundColor: '#001025',
-      })
+        color: "white",
+        backgroundColor: "#001025",
+      });
     } else {
-      setText('black')
-      setMode('light')
-      document.body.style.color = 'black'
-      document.body.style.backgroundColor = 'white'
-      showAlert('Light mode has been enabled!!', 'success')
-      document.title = 'Light Mode'
+      setText("black");
+      setMode("light");
+      document.body.style.color = "black";
+      document.body.style.backgroundColor = "white";
+      showAlert("Light mode has been enabled!!", "success");
+      document.title = "Light Mode";
 
       setMyStyle({
-        color: 'black',
-        backgroundColor: 'white',
-      })
+        color: "black",
+        backgroundColor: "white",
+      });
     }
-  }
+  };
 
-
-
+  // setProgress(10);
 
   return (
     <>
       <Router>
-        <Navbar title="My Site" toggleMode={changeTheme} bgColor={mode} text={text} />
+        <LoadingBar color="#f11946" height={5} progress={100} />
+        <Navbar
+          title="My Site"
+          toggleMode={changeTheme}
+          bgColor={mode}
+          text={text}
+          setProgress={setProgress}
+        />
         <Alert alert={alert} />
         <div className="container my-5">
           <Routes>
             <Route exact path="/home" element={<Home />} />
             <Route exact path="/about" element={<About myStyle={myStyle} />} />
-            <Route exact path="/text" element={<Textform heading="Enter the text to analyze" showAlert={showAlert} />} />
+            <Route
+              exact
+              path="/text"
+              element={
+                <Textform
+                  heading="Enter the text to analyze"
+                  showAlert={showAlert}
+                />
+              }
+            />
           </Routes>
         </div>
       </Router>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
--- src/components/Textform.jsx
+++ src/components/Textform.jsx
@@ -1,66 +1,78 @@
-import React, { useState } from 'react'
+import React, { useState } from "react";
 
 function Textform(props) {
-	const [text, setText] = useState('Enter text here')
-	let time = 0
+  const [text, setText] = useState("Enter text here");
+  let time = 0;
 
-	const handleClick = () => {
-		console.log("Clicked " + text)
-		let newText = text.toUpperCase()
-		setText(newText)
-		props.showAlert("Changed to uppercase", 'success')
-	}
+  const handleClick = () => {
+    console.log("Clicked " + text);
+    let newText = text.toUpperCase();
+    setText(newText);
+    props.showAlert("Changed to uppercase", "success");
+  };
 
-	const handleOnChange = (e) => {
-		setText(e.target.value)
-	}
+  const handleOnChange = (e) => {
+    setText(e.target.value);
+  };
 
-	const emptyArea = () => {
-		setText('')
-	}
-	const copyText = () => {
-		let text = document.getElementById("myBox")
-		text.select()
-		navigator.clipboard.writeText(text.value)
-	}
-	const handleExtraSpaces = () => {
-		let newText = text.split(/[ ]+/);
-		setText(newText.join(" "))
-	}
-	return (
-		<>
-			<div className="container">
-				<h1 className='text-center py-5 text-bold'>
-					{props.heading}
-				</h1>
-				<div className="mb-3">
-					<textarea className="form-control"
-						value={text} onChange={handleOnChange}
-						onClick={emptyArea} id="myBox" rows="8">
-					</textarea>
-				</div>
-				<button className="btn btn-info text-white"
-					onClick={handleClick} >
-					Convert to Uppercase
-				</button>
-				<button className="btn btn-primary text-white mx-4"
-					onClick={copyText}>
-					Copy Text
-				</button>
-				<button className="btn btn-primary text-white mx-4"
-					onClick={handleExtraSpaces}>
-					Handle Spaces
-				</button>
-			</div>
+  const emptyArea = () => {
+    setText("");
+  };
+  const copyText = () => {
+    let text = document.getElementById("myBox");
+    text.select();
+    navigator.clipboard.writeText(text.value);
+  };
+  const handleExtraSpaces = () => {
+    let newText = text.split(/[ ]+/);
+    setText(newText.join(" "));
+  };
+  return (
+    <>
+      <div className="container">
+        <h1 className="text-center py-5 text-bold">{props.heading}</h1>
+        <div className="mb-3">
+          <textarea
+            className="form-control"
+            value={text}
+            onChange={handleOnChange}
+            spellCheck="true"
+            onClick={emptyArea}
+            id="myBox"
+            rows="8"
+          ></textarea>
+        </div>
+        <button className="btn btn-info text-white" onClick={handleClick}>
+          <a href="/" accessKey="c">
+            Convert to Uppercase
+          </a>
+        </button>
+        <button className="btn btn-primary text-white mx-4" onClick={copyText}>
+          Copy Text
+        </button>
+        <button
+          className="btn btn-primary text-white mx-4"
+          onClick={handleExtraSpaces}
+        >
+          Handle Spaces
+        </button>
+      </div>
 
-			<div className="container my-2">
-				<h1>Your Text Summary</h1>
-				<p>{text.split(" ").length} words {text.length} characters.</p>
-				<p>{0.008 * text.split(" ").length} minutes to read.</p>
-				<h2>Preview</h2>
-				<p>{text}</p>
-			</div>
-		</>
-	)
+      <div className="container my-2">
+        <h1>Your Text Summary</h1>
+        <p>
+          {
+            text.split(" ").filter((element) => {
+              return element.length != 0;
+            }).length
+          }{" "}
+          words {text.length} characters.
+        </p>
+        <p>{0.008 * text.split(" ").length} minutes to read.</p>
+        <h2>Preview</h2>
+        <p>{text}</p>
+      </div>
+    </>
+  );
 }
-export default Textform
+export default Textform;
